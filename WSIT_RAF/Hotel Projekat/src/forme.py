99ebe9cebaef7845a4960f8db921b9e810c7220e
Stefan Petrović <56268829+btstefan@users.noreply.github.com>
2020-03-13 20:11:35 +0100

    Add files via upload

--- WSIT_RAF/Hotel Projekat/src/forme.py
+++ WSIT_RAF/Hotel Projekat/src/forme.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, FileField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, FileField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import Form
@@ -7,44 +7,44 @@ from wtforms.widgets import html5
 from wtforms.fields.html5 import DateField
 
 class registracijaForm(FlaskForm):
-	username = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Korisničko ime"})
-	ime_prezime = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Ime i prezime"})
-	email = StringField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Email adresa"})
-	godina_rodjenja = IntegerField('Godina rodjenja', widget=html5.NumberInput(), render_kw={"placeholder" : "Godina Rodjenja"})
-	password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Lozinka"})
-	confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Potvrdi lozinku"})
-	telefon = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Telefon"})
-	slika = FileField("Profilna slika", validators=[FileAllowed(['jpg', 'png'])])
-	submit = SubmitField('Registruj se', render_kw={"id": "reg"})
+	r_username = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Username"})
+	r_ime_prezime = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Name and surname"})
+	r_email = StringField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Email address"})
+	r_godina_rodjenja = IntegerField('Godina rodjenja', widget=html5.NumberInput(), render_kw={"placeholder" : "Year of birth"})
+	r_password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Password"})
+	r_confirm_password = PasswordField(validators=[DataRequired(), EqualTo('r_password')], render_kw={"placeholder": "Confirm password"})
+	r_telefon = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Phone number"})
+	r_slika = FileField("Profile photo", validators=[FileAllowed(['jpg', 'png'])])
+	r_submit = SubmitField('Sign Up', render_kw={"id": "reg"})
 
 class loginForm(FlaskForm):
 	username = StringField(validators=[DataRequired()], render_kw={"placeholder": "Username"})
-	password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Lozinka"})
-	submit = SubmitField('Prijavi se')
+	password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Password"})
+	submit = SubmitField('Login')
 
 class admin_loginForm(FlaskForm):
 	username = StringField(validators=[DataRequired()], render_kw={"placeholder": "Username"})
-	password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Lozinka"})
-	submit = SubmitField('Prijavi se')
+	password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Password"})
+	submit = SubmitField('Login')
 
 class rezervacijaForm(FlaskForm):
-	ime_prezime = StringField(validators=[DataRequired(), Length(min=2, max=30)], render_kw={"placeholder": "Ime i prezime"})
-	email = StringField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Email adresa"})
-	telefon = StringField(validators=[DataRequired(), Length(min=2, max=30)], render_kw={"placeholder": "Telefon"})
-	submit = SubmitField('Rezerviši sobu')
+	ime_prezime = StringField(validators=[DataRequired(), Length(min=2, max=30)], render_kw={"placeholder": "Name and surname"})
+	email = StringField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Email address"})
+	telefon = StringField(validators=[DataRequired(), Length(min=2, max=30)], render_kw={"placeholder": "Phone number"})
+	submit = SubmitField('Make reservation')
 
 class dodajSobu(FlaskForm):
-	broj_sobe = IntegerField(widget=html5.NumberInput(), render_kw={"placeholder" : "Broj sobe"})
-	broj_kreveta = IntegerField(widget=html5.NumberInput(), render_kw={"placeholder" : "Broj kreveta"})
-	cena_nocenja = IntegerField(widget=html5.NumberInput(), render_kw={"placeholder" : "Cena noćenja"})
-	submit = SubmitField('Dodaj sobu')
+	broj_sobe = IntegerField(widget=html5.NumberInput(), render_kw={"placeholder" : "Room number"})
+	broj_kreveta = IntegerField(widget=html5.NumberInput(), render_kw={"placeholder" : "Bed count"})
+	cena_nocenja = IntegerField(widget=html5.NumberInput(), render_kw={"placeholder" : "Price per night"})
+	submit = SubmitField('Add room')
 
 class updateProfil(FlaskForm):
-	ime_prezime = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Ime i prezime"})
-	email = StringField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Email adresa"})
-	godina_rodjenja = IntegerField('Godina rodjenja', widget=html5.NumberInput(), render_kw={"placeholder" : "Godina Rodjenja"})
-	password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Lozinka"})
-	confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Potvrdi lozinku"})
-	telefon = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Telefon"})
-	slika = FileField("Profilna slika", validators=[FileAllowed(['jpg', 'png'])])
-	submit = SubmitField('Izmeni profil')
+	ime_prezime = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Name and surname"})
+	email = StringField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Email address"})
+	godina_rodjenja = IntegerField('Godina rodjenja', widget=html5.NumberInput(), render_kw={"placeholder" : "Year of birth"})
+	password = PasswordField(validators=[DataRequired()], render_kw={"placeholder": "Password"})
+	confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Confirm password"})
+	telefon = StringField(validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Phone number"})
+	slika = FileField("Profile photo", validators=[FileAllowed(['jpg', 'png'])])
+	submit = SubmitField('Edit profile')
--- WSIT_RAF/Hotel Projekat/src/main.py
+++ WSIT_RAF/Hotel Projekat/src/main.py
@@ -2,19 +2,29 @@ from flask import Flask, render_template, flash, redirect, url_for, request, ses
 from forme import registracijaForm, loginForm, admin_loginForm, rezervacijaForm, dodajSobu, updateProfil
 from pymongo import MongoClient
 from flask_mail import Mail
+from flask_mail import Message
 from flask_uploads import UploadSet, IMAGES, configure_uploads
-from datetime import datetime
+from datetime import datetime, date
 from bson import ObjectId
-import hashlib
-import time
-import datetime
-import string
-import random
+import hashlib, time, string, random
+from PIL import Image
+import os
+import secrets
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pSkJHDRJNMNv9X9Fb83KXJf8u8'
 app.config['UPLOADED_PHOTOS_DEST'] = 'src'
 app.config['UPLOAD_FOLDER'] = 'static'
+
+# MAIL konfiguracija
+app.config.update(
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = "srbwebshop@gmail.com",
+	MAIL_PASSWORD = "rodjak555"
+	)
+mail = Mail(app)
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
@@ -25,6 +35,19 @@ admins = db["admin"]
 rezervacije = db["rezervacije"]
 sobe = db["sobe"]
 
+def upload_sliku(slika, korisnik):
+    ime_rnd = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(9))
+    #_, f_ext = os.path.splitext(slika.filename)
+    slika_ime = korisnik + '_' + ime_rnd + '.jpg'
+    slika_putanja = os.path.join(app.root_path, 'static/img/korisnici', slika_ime)
+
+    velicina = (200, 200)
+    s = Image.open(slika)
+    s.thumbnail(velicina, Image.ANTIALIAS)
+    s.save(slika_putanja)
+
+    return slika_ime
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	form = registracijaForm()
@@ -32,31 +55,38 @@ def index():
 	rezervacija = rezervacijaForm()
 
 	if form.validate_on_submit():
-		postojeci_korisnik = users.find_one({"korisnicko_ime": form.username.data})
+		postojeci_korisnik = users.find_one({"korisnicko_ime": form.r_username.data})
 		if postojeci_korisnik is not None:
-			flash(f'Korisnicko ime \"{form.username.data}\" je zauzeto', 'danger')
+			flash(f'Username \"{form.r_username.data}\" already exist', 'danger')
 		else:
-			if form.slika.data is None:
+			if form.r_slika.data is None:
 				putanja = "/static/img/korisnici/default.png"
 			else:
-				ime_rnd = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(9))
-				photos.save(form.slika.data, 'static/img/korisnici', form.username.data + '_' + ime_rnd + '.jpg')
-				putanja = "/static/img/korisnici/" + form.username.data + '_' + ime_rnd + ".jpg"
+				putanja = "/static/img/korisnici/" + upload_sliku(form.r_slika.data, form.r_username.data)
 
-			hash_pw = hashlib.sha256(form.password.data.encode())
+				#ime_rnd = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(9))
+				#photos.save(i, 'static/img/korisnici', form.r_username.data + '_' + ime_rnd + '.jpg')
+				#putanja = "/static/img/korisnici/" + form.r_username.data + '_' + ime_rnd + ".jpg"
+
+			hash_pw = hashlib.sha256(form.r_password.data.encode())
 			password_hashed = hash_pw.hexdigest()
 			korisnik = {
-				"korisnicko_ime":form.username.data,
-				"ime_prezime":form.ime_prezime.data,
-				"email":form.email.data,
-				"godina_rodjenja":form.godina_rodjenja.data,
+				"korisnicko_ime":form.r_username.data,
+				"ime_prezime":form.r_ime_prezime.data,
+				"email":form.r_email.data,
+				"godina_rodjenja":form.r_godina_rodjenja.data,
 				"password":password_hashed,
-				"telefon":form.telefon.data,
+				"telefon":form.r_telefon.data,
 				"slika":putanja,
 				"registrovan":time.strftime("%d.%m.%Y %H:%M")
 			}
 			users.insert_one(korisnik)
-			flash(f'{form.username.data} Uspešno ste se registrovali!')
+			k = users.find_one({'korisnicko_ime':form.r_username.data, 'password':password_hashed})
+			if k is not None:
+				session['_id'] = str(k['_id'])
+				session['usr'] = k['korisnicko_ime']
+				session['slika'] = k['slika']
+			flash('Registration was successful!')
 			return redirect(url_for('index'))
 
 
@@ -66,7 +96,7 @@ def index():
 def login():
 	login = loginForm()
 	if '_id' in session:
-		flash(f"Već ste ulogovani kao: {session['usr']}");
+		flash(f"You are already logged in: {session['usr']}");
 		return redirect(url_for('index'))
 
 	hash_pw = hashlib.sha256(login.password.data.encode())
@@ -74,14 +104,13 @@ def login():
 	korisnik = users.find_one({'korisnicko_ime':login.username.data, 'password':password_hashed})
 
 	if korisnik is None:
-		flash('Neuspešan login. Pogrešan username ili password')
+		flash('Wrong username or password')
 	else:
 		session['_id'] = str(korisnik['_id'])
 		session['usr'] = korisnik['korisnicko_ime']
 		session['slika'] = korisnik['slika']
 		if 'rezervisao' in session:
 			session.pop('rezervisao', None)
-		flash('Uspešno ste se ulogovali!', 'success')
 	return redirect(url_for('index'))
 
 @app.route('/logout')
@@ -90,10 +119,9 @@ def logout():
 		session.pop('_id', None)
 		session.pop('usr', None)
 		session.pop('slika', None)
-		flash("Uspešno ste se odjavili sa profila!")
 		return redirect(url_for('index'))
 	else: 
-		flash("Niste ulogovani!")
+		flash("You are not logged in!")
 		return redirect(url_for('index'))
 
 @app.route('/admin', methods=['POST', 'GET'])
@@ -109,11 +137,10 @@ def admin():
 		admin = admins.find_one({'ime_admina':admin_forma.username.data, 'lozinka_admina':password_hashed})
 		
 		if admin is None:
-			flash('Neuspešan login. Pogrešan username ili password!')
+			flash('Wrong username or password!')
 		else:
 			session['admin_id'] = str(admin['_id'])
 			session['admin_ime'] = admin['ime_admina']
-			flash('Uspešno ste se ulogovali!', 'success')
 			return redirect(url_for('admin_panel'))
 
 	return render_template("admin_login.html", admin=admin_forma)
@@ -123,17 +150,16 @@ def admin_logout():
 	if "admin_id" in session:
 		session.pop('admin_id', None)
 		session.pop('admin_ime', None)
-		flash("Uspešno ste se odjavili sa admin panela!")
 		return redirect(url_for('admin'))
 	else: 
-		flash("Niste ulogovani kao admin!")
+		flash("You are not logged in as admin!")
 		return redirect(url_for('admin'))
 
 @app.route('/admin/panel', methods=['POST', 'GET'])
 def admin_panel():
 	soba = dodajSobu();
 	if "admin_id" not in session:
-		flash("Morate biti ulogovani kao admin!")
+		flash("You must be logged in as admin!")
 		return redirect(url_for('admin'))
 		
 	k = list(users.find()) #svi korisnici
@@ -142,17 +168,17 @@ def admin_panel():
 	s = list(sobe.find()) #sve sobe
 
 	if soba.validate_on_submit():
-		sb = sobe.find_one({"broj_sobe":soba.broj_sobe.data})
-		if sb is not None: 
-			flash("Broj sobe već postoji")
-			return redirect(url_for('admin'))
-		s = {
-			"broj_sobe":str(soba.broj_sobe.data),
-			"broj_kreveta":str(soba.broj_kreveta.data),
-			"cena_nocenja":soba.cena_nocenja.data
-		}
-		sobe.insert_one(s)
-		flash("Uspešno ste dodali sobu")
+		sb = sobe.find_one({"broj_sobe":str(soba.broj_sobe.data)})
+		if sb is None: 
+			s = {
+				"broj_sobe" : str(soba.broj_sobe.data),
+				"broj_kreveta" : str(soba.broj_kreveta.data),
+				"cena_nocenja" : soba.cena_nocenja.data
+			}
+			sobe.insert_one(s)
+			flash("Room added successfuly!")
+		else:
+			flash(f"Room number {soba.broj_sobe.data} already exist")
 		return redirect(url_for('admin'))
 
 	return render_template('admin_panel.html', k=k, a=a, r=r, s=s, soba=soba)
@@ -168,8 +194,12 @@ def rezervisi():
 	datum1_date = time.strptime(datum1, "%Y-%m-%d")
 	datum2_date = time.strptime(datum2, "%Y-%m-%d")
 
+	d1 = datetime.strptime(datum1, "%Y-%m-%d")
+	d2 = datetime.strptime(datum2, "%Y-%m-%d")
+	razlika = abs((d2 - d1).days)
+	
 	if datum2_date <= datum1_date:
-		return jsonify({'error' : 'Datum odjave mora biti veci od datuma prijave' })
+		return jsonify({'error' : 'CHECK-OUT date must be after CHECK-IN date' })
 
 	if datum1 and datum2 and osobe:
 		lista_soba = sobe.find({"broj_kreveta" : str(osobe)})
@@ -193,16 +223,24 @@ def rezervisi():
 				session['datum1'] = datum1
 				session['datum2'] = datum2
 				session['brojSobe'] = soba['broj_sobe']
-				return jsonify({'poruka' : 'Postoji slobodna soba' + osobe})
+				total = razlika * int(soba['cena_nocenja'])
+				session['total'] = total
+				return jsonify({
+					'poruka' : 'There is a room available.', 
+					'soba' : soba['broj_sobe'], 
+					'cena' : soba['cena_nocenja'],
+					'dani' : str(razlika),
+					'total' : total
+				})
 
-		return jsonify({'error' : 'Zadati termin nije dostupan' + osobe})
+		return jsonify({'error' : 'There is no room available in selected period.'})
 	else: 
-		return jsonify({'error' : 'Popunite sva polja'})
+		return jsonify({'error' : 'All fields are required!'})
 
 @app.route('/rezervacija', methods=['POST'])
 def rezervacija():
 	if 'rezervisao' in session:
-		flash('Potrebno je da se registrujete za više od jedne rezervacije')
+		flash('For more than one reservation, you need to LOGIN.')
 		return redirect(url_for('index'))
 	form = rezervacijaForm()
 
@@ -227,62 +265,64 @@ def rezervacija():
 	}
 
 	rezervacije.insert_one(rezervacija)
-	flash(f"Uspešno ste napravili rezervaciju za sobu broj {rezervacija['soba_id']}")
+	flash(f"Reservation created for room #{rezervacija['soba_id']}")
 	return redirect(url_for('index'))
 
 @app.route('/obrisi/sobu/<id>', methods=['POST', 'GET'])
 def obrisi_sobu(id):
 	if "admin_id" not in session:
-		flash("Morate biti ulogovani kao admin!")
+		flash("You must be logged in as admin!")
 		return redirect(url_for('admin'))
 	try: 
 		ObjectId(id)
 		soba = sobe.find_one({"_id":ObjectId(id)})
 
 		sobe.remove(soba)
-		flash("Soba je uspešno obrisana!")
+		flash("Room deleted!")
 		return redirect(url_for('admin'))
 	except: 
-		flash(f"Greška ID: \"{ id }\" nije validan!");
+		flash(f"Error: \"{ id }\" is not a valid id!");
 		return redirect(url_for('admin'))
 
 @app.route('/obrisi/korisnika/<id>', methods=['POST', 'GET'])
 def obrisi_korisnika(id):
 	if "admin_id" not in session:
-		flash("Morate biti ulogovani kao admin!")
+		flash("You must be logged in as admin!")
 		return redirect(url_for('admin'))
 	try: 
 		ObjectId(id)
 		korisnik = users.find_one({"_id":ObjectId(id)})
 
 		users.remove(korisnik)
-		flash("Korisnik je uspešno obrisan!")
+		flash("User deleted!")
 		return redirect(url_for('admin'))
 	except: 
-		flash(f"Greška ID: \"{ id }\" nije validan!");
+		flash(f"Error: \"{ id }\" is not a valid id!");
 		return redirect(url_for('admin'))
+
 
 @app.route('/obrisi/rezervaciju/<id>', methods=['POST', 'GET'])
 def obrisi_rezervaciju(id):
 	if "admin_id" not in session:
-		flash("Morate biti ulogovani kao admin ili korisnik!")
+		flash("You must be logged in")
 		return redirect(url_for('admin'))
 	try: 
 		ObjectId(id)
 		rezervacija = rezervacije.find_one({"_id":ObjectId(id)})
 
 		rezervacije.remove(rezervacija)
-		flash("Rezervacija je uspešno obrisana!")
+		flash("Reservation canceled!")
 		return redirect(url_for('admin'))
 	except: 
-		flash(f"Greška ID: \"{ id }\" nije validan!");
+		flash(f"Error: \"{ id }\" is not a valid id!");
 		return redirect(url_for('admin'))
+
 
 @app.route('/profil/<id>', methods=['POST', 'GET'])
 def profil(id):
 	if "_id" not in session:
 		if "admin_id" not in session:
-			flash("Morate biti ulogovani!")
+			flash("You must be logged in!")
 			return redirect(url_for('index'))
 
 	korisnik = users.find_one({"_id":ObjectId(id)})
@@ -312,18 +352,24 @@ def profil(id):
 				"registrovan":time.strftime("%d.%m.%Y %H:%M")
 			}
 			users.update_one({"_id":ObjectId(id)}, {"$set":user})
-			flash("Uspešno ste izmenili profil")
+			flash("Your profile was changed!")
 			return redirect(request.url)
-		ima = 1
-		lista_rezervacija = rezervacije.find({"korisnik_id":id})
-		if lista_rezervacija is None:
-			ima = 0
-		return render_template("profil.html", korisnik=korisnik, k=form, lista_rezervacija=lista_rezervacija, ima=ima)
+
+		lista_rezervacija = []
+		show = True
+		if rezervacije.find_one({"korisnik_id":id}) is None:
+			show = False
+
+		if show:
+			lista_rezervacija = rezervacije.find({"korisnik_id":id})
+		
+		return render_template("profil.html", korisnik=korisnik, k=form, lista_rezervacija=lista_rezervacija, show=show)
 
 	else:
-		flash("Niste ulogovani kao korisnik!")
+		flash("You are not logged in as user!")
 
 	return redirect(url_for('index'))
+
 
 @app.route('/obrisi/rezervaciju/korisnik/<id>', methods=['POST', 'GET'])
 def obrisi_rezervaciju_k(id):
@@ -333,16 +379,30 @@ def obrisi_rezervaciju_k(id):
 			rezervacija = rezervacije.find_one({"_id":ObjectId(id)})
 			if(rezervacija['korisnik_id'] == session['_id']):
 				rezervacije.remove(rezervacija)
-				flash("Rezervacija je uspešno obrisana!")
+				flash("Reservation deleted!")
 				return redirect(url_for('index'))
-			flash("Nemate pristup ovoj rezervaciji!")
+			flash("Access denied!")
 			return redirect(url_for('index'))
 		except: 
-			flash(f"Greška ID: \"{ id }\" nije validan!");
+			flash(f"Error: \"{ id }\" is not a valid id!");
 			return redirect(url_for('index'))
 
-	flash("Niste ulogovani!")
+	flash("You are not logged in!")
 	return redirect(url_for('index'))
+
+@app.route('/kontakt', methods=['POST'])
+def kontakt():
+	ime = request.form['ime_prezime']
+	email = request.form['email']
+	poruka = request.form['poruka']
+	
+	if ime and email and poruka:
+		msg = Message('Hotel contact', sender='savadunav5@gmail.com', recipients=['stefanpetrovicsrb@gmail.com'])
+		msg.html = '<b>Hotel contact form<br/><br/>Name:</b> ' + ime + '<br/>' + '<b>Email:</b> ' + email + '<br/>' + '<b>Message:</b><br/>' + poruka
+		mail.send(msg)
+		return jsonify({'poruka' : 'success'})
+	
+	return jsonify({'error' : 'error'})
 
 if __name__ == '__main__':
 	app.run(debug=True)
